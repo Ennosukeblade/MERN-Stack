34568c45db5fc28a8aca6a7f697338000dae80a6
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-03-13 16:15:11 +0100

    Pokemon API

---
+++ W2D1_React_APIs/Practice/pokemon-api/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [pokemons, setPokemons] = useState([]);
+    const fetchPokemons = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon/")
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+              console.log(response.results)
+              setPokemons(response.results)
+    })
+            .catch((err) => console.log(err));
+    };
+    return (
+        <div className="App">
+            <button onClick={fetchPokemons}>Fetch Pokemons</button>
+            <ul>
+              { pokemons.map((pokemon,idx) => <li key={idx}>{pokemon.name}</li>) }
+            </ul>
+        </div>
+    );
+}
+
+export default App;
